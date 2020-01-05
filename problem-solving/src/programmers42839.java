0c52018b720c24ffba0d73973755d913d1475930
doodo1118 <doodo1118@gmail.com>
2020-01-06 00:11:29 +0900

    programmers - findPrimeNumber

---
+++ problem-solving/src/programmers42839.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class programmers42839 {
+	static int inputLength;
+	static String input;
+	static Set<Integer> candidateNumbers;
+	static boolean[] isUsed;
+	
+	public static void main(String args[]) {
+		Scanner s = new Scanner(System.in);
+		input = s.next();
+		
+		candidateNumbers = new HashSet<Integer>();
+		
+		inputLength = input.length();
+		boolean[] isUsed = new boolean[inputLength];
+		
+		for(int howManyToAppend=1; howManyToAppend<=inputLength; howManyToAppend++) {
+			generateNumber(howManyToAppend, "", Arrays.copyOf(isUsed, inputLength), 1);
+		}
+		
+
+		candidateNumbers.remove(1);
+		candidateNumbers.remove(0);
+
+		Iterator<Integer> it = candidateNumbers.iterator();
+		
+		int candidateNumber;
+		int noSosuCount=0;
+		
+		while( it.hasNext() ) {
+			candidateNumber = it.next();
+			for(int i=2; i*i<=candidateNumber; i++) {
+				if( candidateNumber%i==0 ) {
+					//candidateNumbers.remove(number);
+					noSosuCount++;
+					System.out.println( candidateNumber );
+					break;
+				}
+			}
+		}
+		
+		System.out.print( candidateNumbers.size() - noSosuCount);
+	}
+	
+	public static void generateNumber(int howManyToAppend, String numberBeforeAppend, boolean[] isUsed, int depth) {
+		if(howManyToAppend < 1)  return; 
+		
+		for(int i=0; i<inputLength; i++) {
+			if(isUsed[i]==false) {
+				isUsed[i] = true;
+				String numberAfterAppend = numberBeforeAppend + input.charAt(i);
+				candidateNumbers.add( Integer.parseInt(numberAfterAppend) );
+				generateNumber(howManyToAppend-1, numberAfterAppend, Arrays.copyOf(isUsed, inputLength), depth+1);
+				isUsed[i] = false;
+			}
+		}
+	}
+}
