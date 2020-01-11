1ca2b62dfdcfd3bff89d16e93bab4adf2260e121
doodo1118 <doodo1118@gmail.com>
2020-01-11 20:37:53 +0900

    algospot PICNIC

--- problem-solving/src/algospot_PICNIC.java
+++ problem-solving/src/algospot_PICNIC.java
@@ -1,17 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Stack;
 
-public class algospot_PICNIC {
+class algospot_PICNIC {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		//T = testcase
-		int T = Integer.parseInt( bf.readLine() );
+		String input = bf.readLine().trim();
+		int T = Integer.parseInt(input);
+
+		
 		for(int i=0; i<T; i++) {
 			String[] chunks = bf.readLine().split(" ");
 			int numOfChildren = Integer.parseInt( chunks[0] );
@@ -19,41 +20,58 @@ public class algospot_PICNIC {
 			
 			boolean[][] relationMap = new boolean[numOfChildren][numOfChildren];
 		
+			
 			String[] relationInformation = bf.readLine().split(" ");
 			
 			//initiate relationMap
-			for(int j=0; 2*j<numOfRelationInformation; j++) {
-				int child1 = Integer.parseInt( relationInformation[j] );
-				int child2 = Integer.parseInt( relationInformation[j+1] );
+			for(int j=0; j<numOfRelationInformation; j++) {
+				int child1 = Integer.parseInt( relationInformation[2*j] );
+				int child2 = Integer.parseInt( relationInformation[2*j+1] );
 				
 				recordRelation(relationMap, child1, child2);
 			}
 			
 
+			int result=0;
+			
+			HashSet<Integer> childrenToBePaired = new HashSet<Integer>();
+			for(int m=0; m<numOfChildren; m++)
+				childrenToBePaired.add(m);
+			
 			//CurrentState contains hasPartner and currentLocation
-			Stack<CurrentState> stack = new Stack<CurrentState>();
+			Stack<State> stateStack = new Stack<State>();
+			stateStack.add( new State(childrenToBePaired, new int[]{0, 0}) );
 			
-			boolean[] hasPartner = new boolean[numOfChildren];
-			stack.push(new CurrentState( Arrays.copyOf(hasPartner, numOfChildren), new int[] {0,0}));
-			
-			while( !stack.isEmpty() ) {
-				CurrentState cs = stack.pop();
+			while( !stateStack.isEmpty() ) {
+				State currentState = stateStack.pop();
+				boolean flag = false;
 				
-				for(int child1=cs.currentLocation[0]; child1<numOfChildren; child1++) {
-					if ( cs.hasPartner[child1] ) continue;
+				for(int child1=currentState.currentLocation[0]; child1<numOfChildren; child1++) {
+					if ( currentState.hasPartner(child1) ) continue;
 					
-					for(int child2=cs.currentLocation[1]; child2<numOfChildren; child2++) {
-						if( theyAreFriends(relationMap, child1, child2) ) {
+					if(flag) {currentState.currentLocation[1]=0;}
+					flag=true;
+					
+					for(int child2=currentState.currentLocation[1]; child2<numOfChildren; child2++) {
+						if(child1 >= child2) continue;
+						if( theyAreFriends(relationMap, child1, child2)&&!(currentState.hasPartner(child2)) ) {
 							
 							//pair child1 and child2
-							cs.hasPartner[child1] = true;
-							cs.hasPartner[child2] = true;
+							HashSet<Integer> clone = (HashSet<Integer>) currentState.childrenToBePaired.clone();
+							stateStack.push( new State( clone, new int[] {child1, child2+1} ) );
+							currentState.childrenToBePaired.remove(child1);
+							currentState.childrenToBePaired.remove(child2);
+
+							if( currentState.childrenToBePaired.isEmpty() ) result++;
 							
-							stack.push( new CurrentState(Arrays.copyOf(cs.hasPartner, numOfChildren), new int[] {child1, child2}) );
+							break;
 						}
 					}
+					
 				}
 			}
+			
+			System.out.println(result);
 		}
 	}
 	public static void recordRelation(boolean[][] relationMap, int child1, int child2) {
@@ -66,13 +84,17 @@ public class algospot_PICNIC {
 		else return false;
 	}
 	
-	static class CurrentState {
-		boolean[] hasPartner;
+	static class State {
+		HashSet<Integer> childrenToBePaired;
 		int[] currentLocation;
 		
-		CurrentState(boolean[] hasPartner2, int[] currentLocation) {
-			this.hasPartner = hasPartner2;
+		State(HashSet<Integer> childrenToBePaired, int[] currentLocation) {
+			this.childrenToBePaired = childrenToBePaired;
 			this.currentLocation = currentLocation;
+		}
+		
+		boolean hasPartner(int child) {
+			return !this.childrenToBePaired.contains(child);
 		}
 	}
 	
